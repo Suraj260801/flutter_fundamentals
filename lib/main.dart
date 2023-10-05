0b62b2b7cf1ba20fc79ab5aeddb50876003296bb
Suraj Suryawanshi <101798684+Suraj260801@users.noreply.github.com>
2023-10-05 17:32:52 +0530

    added boiler plate code to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:research/blocks/internet_bloc/internet_bloc.dart';
-import 'package:research/blocks/internet_bloc/internet_state.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,16 +10,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<InternetBloc>(
-      create: (BuildContext context) => InternetBloc(),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -37,36 +31,6 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Testing'),
-      ),
-      body: Center(
-        child: BlocConsumer<InternetBloc, InternetState>(
-          builder: (BuildContext context, state) {
-            if (state is InternetGainedState) {
-              return const Text(
-                'Connected',
-                style: TextStyle(fontSize: 40),
-              );
-            } else if (state is InternetLostState) {
-              return const Text('Not Connected',
-                  style: TextStyle(fontSize: 40));
-            } else {
-              return const Text('Loading...', style: TextStyle(fontSize: 40));
-            }
-          },
-          listener: (BuildContext context, InternetState state) {
-            if (state is InternetGainedState) {
-              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                content: Text('Device Connected to Internet ',
-                    style: TextStyle(fontSize: 15)),backgroundColor: Colors.green,
-              ));
-            }else if(state is InternetLostState){
-              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                content: Text('Device Not Connected to Internet',
-                    style: TextStyle(fontSize: 15)),backgroundColor: Colors.red,
-              ));
-            }
-          },
-        ),
       ),
     );
   }
