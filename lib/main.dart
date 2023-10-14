aebc9ebb21283995b3b6dee4ca585ac5590c2168
Suraj Suryawanshi <101798684+Suraj260801@users.noreply.github.com>
2023-10-14 20:04:39 +0530

    responsive UI using aspect ratio

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,63 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Testing'),
       ),
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.center,
+            constraints: const BoxConstraints(),
+            width: double.infinity,
+            height: 200,
+            color: Colors.blue,
+            child: AspectRatio(
+              //h=200 because w=infinity
+              //w=200*16/9=355.5<411.4(device width)
+              aspectRatio: 2,
+              child: Container(
+                color: Colors.green,
+              ),
+            ),
+          ),
+          const Divider(
+            thickness: 1,
+          ),
+          Container(
+            alignment: Alignment.center,
+            constraints: const BoxConstraints(),
+            width: double.infinity,
+            height: 200,
+            color: Colors.blue,
+            child: AspectRatio(
+              //h=200 because w=infinity
+              //w=200*16/9=600>411.4(device width)
+              //w=411.4(max allowed from parent)
+              //height=411.4/3=137.3
+              aspectRatio: 3,
+              child: Container(
+                color: Colors.green,
+              ),
+            ),
+          ),
+          const Divider(
+            thickness: 1,
+          ),
+          Container(
+            alignment: Alignment.center,
+            constraints: const BoxConstraints(),
+            width: 150,
+            height: 150,
+            color: Colors.blue,
+            child: AspectRatio(
+              //w=150 because w=fixed&&<deviceWidth
+              //h=150/2=75<(device height)
+              aspectRatio: 2,
+              child: Container(
+                color: Colors.green,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
